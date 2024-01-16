e4f62ca2f8362301207d1d39a04fbca92900159b
Zhichao Link <sunzhichao@minijoy.work>
2024-01-16 19:01:48 +0800

    update to 0.2.16

--- Demo/Demo/ViewControllers/LoginListViewController.swift
+++ Demo/Demo/ViewControllers/LoginListViewController.swift
@@ -215,7 +215,7 @@ extension LoginListViewController {
         let adapter = ParticleConnect.getAllAdapters().filter {
             $0.walletType == .particle
         }.first!
-        let config = ParticleAuthConfig(loginType: type, supportAuthType: supportAuthType, phoneOrEmailAccount: nil)
+        let config = ParticleAuthConfig(loginType: type, supportAuthType: supportAuthType, account: nil)
         adapter.connect(config).subscribe { [weak self] result in
             guard let self = self else { return }
             switch result {
